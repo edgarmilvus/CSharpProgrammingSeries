1128c9b33067f5a15e4474d8bf69c5076048809d
Valter Mazzola <vmazzola@eventboost.com>
2026-02-10 10:06:24 +0100

    fix comments

--- Volume_01_Foundations/Chapter_18_chapter_18_constructors_-_strict_object_initialization/00_Theory_Concepts/theory_theoretical_foundations_part11.cs
+++ Volume_01_Foundations/Chapter_18_chapter_18_constructors_-_strict_object_initialization/00_Theory_Concepts/theory_theoretical_foundations_part11.cs
@@ -1,12 +1,9 @@
 
+/*
 #
 # These sources are part of the "C# Programming Series" by Edgar Milvus, 
-# you can find it on stores: 
-# 
-# https://www.amazon.com/dp/B0GKJ3NYL6 or https://tinyurl.com/CSharpProgrammingBooks or 
-# https://leanpub.com/u/edgarmilvus (quantity discounts)
-# 
-# New books info: https://linktr.ee/edgarmilvus 
+# for additional info, new volumes, link to stores:
+# https://github.com/edgarmilvus/CSharpProgrammingSeries
 #
 # MIT License
 # Copyright (c) 2026 Edgar Milvus
@@ -27,10 +24,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+*/
 
-# Source File: theory_theoretical_foundations_part11.cs
-# Description: Theoretical Foundations
-# ==========================================
+// Source File: theory_theoretical_foundations_part11.cs
+// Description: Theoretical Foundations
+// ==========================================
 
 DataPreprocessor dp1 = new DataPreprocessor(500); // MaxWordCount becomes 500
 DataPreprocessor dp2 = new DataPreprocessor(-10); // MaxWordCount becomes 0 (Safe!)
